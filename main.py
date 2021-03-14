1f0657425b8b17b5975c90a24499ace53e51add6
curly1210 <77237708+curly1210@users.noreply.github.com>
2021-03-14 13:49:29 +0700

    update cat anh

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ for idx, con in enumerate(contours):
 def sort_box(box):
     return box[2] * box[3]
 
-
 box_rect = sorted(box_rect, key=sort_box)
 four_max_box = box_rect[-4:]
 w_max_average = int(np.average(list(map(lambda box: box[2], four_max_box))))
@@ -30,17 +29,71 @@ h_max_average = int(np.average(list(map(lambda box: box[3], four_max_box))))
 box_rect = sorted(box_rect, key=lambda box: box[1])
 _, _, w_min, h_min = list(sorted(box_rect, key=lambda box: box[2]))[0]
 
+
+print(w_max_average, h_max_average, w_min, h_min)
+# lấy tọa độ điểm để, cắt ảnh
+sbd = box_rect[2][0], box_rect[2][1], box_rect[5][0], box_rect[5][1]
+ma_de = box_rect[3][0], box_rect[3][1], box_rect[0][0], box_rect[5][1]
+cau_1_den_10 = box_rect[8][0], box_rect[8][1], box_rect[13][0], box_rect[13][1]
+cau_11_den_20 = box_rect[12][0], box_rect[12][1], box_rect[17][0], box_rect[17][1]
+cau_21_den_30 = box_rect[16][0], box_rect[16][1], box_rect[23][0], box_rect[23][1]
+cau_31_den_40 = box_rect[9][0], box_rect[9][1], box_rect[11][0], box_rect[12][1]
+cau_41_den_50 = box_rect[13][0], box_rect[13][1], box_rect[15][0], box_rect[15][1]
+cau_51_den_60 = box_rect[17][0], box_rect[17][1], box_rect[21][0], box_rect[21][1]
+cau_61_den_70 = box_rect[7][0], box_rect[7][1], box_rect[10][0], box_rect[10][1]
+cau_71_den_80 = box_rect[11][0], box_rect[11][1], box_rect[14][0], box_rect[14][1]
+cau_81_den_90 = box_rect[15][0], box_rect[15][1], box_rect[20][0], box_rect[20][1]
+cau_91_den_100 = box_rect[6][0], box_rect[6][1], box_rect[0][0], box_rect[10][1]
+cau_101_den_110 = box_rect[10][0], box_rect[10][1], box_rect[0][0], box_rect[14][1]
+cau_111_den_120 = box_rect[14][0], box_rect[14][1], box_rect[0][0], box_rect[20][1]
+# cắt ảnh
+x1, y1, x2, y2 = sbd
+cv2.imwrite('extra/sbd.png', image[y1 + h_min:y2, x1 + int(w_min * 1.4):x2 - int(w_min * 1.8)])
+x1, y1, x2, y2 = ma_de
+cv2.imwrite('extra/ma_de.png', image[y1 + h_min:y2, x1 + int(w_min * 1.4):x2 - int(w_max_average * 1.4)])
+x1, y1, x2, y2 = cau_1_den_10
+cv2.imwrite('extra/cau_1_den_10.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_11_den_20
+cv2.imwrite('extra/cau_11_den_20.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_21_den_30
+cv2.imwrite('extra/cau_21_den_30.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_31_den_40
+cv2.imwrite('extra/cau_31_den_40.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_41_den_50
+cv2.imwrite('extra/cau_41_den_50.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_51_den_60
+cv2.imwrite('extra/cau_51_den_60.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_61_den_70
+cv2.imwrite('extra/cau_61_den_70.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_71_den_80
+cv2.imwrite('extra/cau_71_den_80.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_81_den_90
+cv2.imwrite('extra/cau_81_den_90.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 1.9)])
+x1, y1, x2, y2 = cau_91_den_100
+cv2.imwrite('extra/cau_91_den_100.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 2.9)])
+x1, y1, x2, y2 = cau_101_den_110
+cv2.imwrite('extra/cau_101_den_110.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 2.9)])
+x1, y1, x2, y2 = cau_111_den_120
+cv2.imwrite('extra/cau_111_den_120.png', image[y1 + int(h_min * 1.3):y2 - int(h_min * 0.3), x1 + int(w_min * 1.4)
+            : x2 - int(w_min * 2.9)])
+
+# cv2.imshow('dap an2',image[y1:y2, x1:x2])
+
 for idx, box in enumerate(box_rect):
     x, y, w, h = box
     cv2.putText(blank_image, str(idx), (x, y), cv2.FONT_HERSHEY_SIMPLEX,
                 0.4, (0, 0, 255),
                 1)
-print(w_max_average, h_max_average, w_min, h_min)
-sbd = box_rect[2][0], box_rect[2][1], box_rect[5][0], box_rect[5][1]
-ma_de = box_rect[3][0], box_rect[3][1], box_rect[0][0], box_rect[5][1]
 cv2.imshow('blank_image', blank_image)
-x1, y1, x2, y2 = sbd
-cv2.imwrite('extra/sbd.png', image[y1 + h_min:y2, x1 + int(w_min * 1.4):x2 - int(w_min * 1.8)])
-x1, y1, x2, y2 = ma_de
-cv2.imwrite('extra/ma_de.png', image[y1 + h_min:y2, x1 + int(w_min * 1.4):x2 - int(w_max_average * 1.4)])
 cv2.waitKey()
